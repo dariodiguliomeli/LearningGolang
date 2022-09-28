34bf2de026de0f050d9e34f75810ea3cb29eadf5
ddigulio <dario.digulio@mercadolibre.com>
2022-09-28 13:01:15 -0300

    Refactor Bakery practice

--- SimpleDataTypes/Bakery.go
+++ SimpleDataTypes/Bakery.go
@@ -7,10 +7,6 @@ import (
 )
 
 func Bakery() {
-	// Una panadería vende barras de pan a $3.49 cada una. El pan que no es el día tiene un descuento del 60%.
-	// Escribir un programa que comience leyendo el número de barras vendidas que no son del día.
-	// Después el programa debe mostrar el precio habitual de una barra de pan,
-	// el descuento que se le hace por no ser fresca y el coste final total.
 	const BREADPRICE = 3.49
 	const OLDBREADDISCOUNT = 0.6
 	oldBreadSold, _ := strconv.ParseFloat(Utils.ScannerInput("Say your bread sold amount: "), 64)
